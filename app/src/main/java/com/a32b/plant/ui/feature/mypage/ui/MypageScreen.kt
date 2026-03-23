1f9a9bb58718118edfc5e99d3775864741407a6f
Your Name <you@example.com>
2026-03-23 10:44:14 +0900

    mypage 수정 확인용

--- app/src/main/java/com/a32b/plant/ui/feature/mypage/ui/MypageScreen.kt
+++ app/src/main/java/com/a32b/plant/ui/feature/mypage/ui/MypageScreen.kt
@@ -2,7 +2,7 @@ package com.a32b.plant.ui.feature.mypage.ui
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-
+// 03-23 김명준 확인용
 @Composable
 fun MypageScreen(navController: NavController){
 
