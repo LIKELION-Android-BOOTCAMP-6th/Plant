b087154e8c80cd20d3a37187b443086345552d9e
son <starlightfjh46@gmail.com>
2026-03-24 22:13:15 +0900

    feat : pot repository 새화분 추가 시 DB로 add 로직

--- app/src/main/java/com/a32b/plant/data/repository/UserRepository.kt
+++ app/src/main/java/com/a32b/plant/data/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.a32b.plant.data.repository
 
+import com.a32b.plant.data.model.PotInfo
 import com.google.firebase.firestore.FirebaseFirestore
 import com.a32b.plant.data.model.UserProfile
 import kotlinx.coroutines.channels.awaitClose
