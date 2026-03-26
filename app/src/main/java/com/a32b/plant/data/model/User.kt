687ab51b03b338214407b98b54164305d5cb1329
taehwan-dev <xkdl1108@gmail.com>
2026-03-26 13:03:36 +0900

    공통소스 인한 merge용 commit

--- app/src/main/java/com/a32b/plant/data/model/User.kt
+++ app/src/main/java/com/a32b/plant/data/model/User.kt
@@ -6,13 +6,15 @@ data class UserProfile(
     val profileImg: String? = null,
     // [추가] 마지막으로 공부를 시작했던 화분의 ID
     val lastSelectedPotId: String? = null,
-    @get:PropertyName("currentPot") @set:PropertyName("currentPot")
-    var currentPot: PotInfo = PotInfo(),
+    val currentPot: PotInfo = PotInfo(),
     // [추가] 사용자가 보유한 화분 전체 리스트
     val potList: List<PotInfo> = emptyList(),
-    val isFirstLogin: Boolean? = null, // 회원가입 시 true 유지 -> 첫 로그인 후 닉네임 재설정 하고 false 바꾸기
-    val isAutoLogin: Boolean? = null,
-    val isDarkMode: Boolean? = null,
+    @get:PropertyName("isFirstLogin") @set:PropertyName("isFirstLogin")
+    var isFirstLogin: Boolean? = null, // 회원가입 시 true 유지 -> 첫 로그인 후 닉네임 재설정 하고 false 바꾸기
+    @get:PropertyName("isAutoLogin") @set:PropertyName("isAutoLogin")
+    var isAutoLogin: Boolean? = null,
+    @get:PropertyName("isDarkMode") @set:PropertyName("isDarkMode")
+    var isDarkMode: Boolean? = null,
     val totalStudyTime: Long? = null,
     val completedPotsCount: Int? = null
 )
