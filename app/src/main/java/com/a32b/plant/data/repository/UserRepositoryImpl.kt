778c134810c9d1c28461830b0d42614b5f8943e9
장지은 <wkdwldms2001@naver.com>
2026-07-09 09:15:24 +0900

    refactor : CurrentUser -> 유저 레포지토리로 이관

--- app/src/main/java/com/a32b/plant/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/a32b/plant/data/repository/UserRepositoryImpl.kt
@@ -2,8 +2,12 @@ package com.a32b.plant.data.repository
 
 import com.a32b.plant.data.datasource.studying.StudyingRemoteDataSource
 import com.a32b.plant.data.datasource.user.UserRemoteDataSource
+import com.a32b.plant.domain.model.User
 import com.a32b.plant.domain.repository.StudyingRepository
 import com.a32b.plant.domain.repository.UserRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,4 +15,14 @@ import javax.inject.Singleton
 class UserRepositoryImpl @Inject constructor(
     private val userRemoteDataSource: UserRemoteDataSource
 ): UserRepository {
+    private val _currentUser = MutableStateFlow<User?>(null)
+
+    override val currentUser: StateFlow<User?> = _currentUser.asStateFlow()
+
+    override fun setCurrentUser(user: User) {
+        _currentUser.value = user
+    }
+    override fun clearCurrentUser() {
+        _currentUser.value = null
+    }
 }
