18e2e7e1b1e684b8dc45b9d4a6b43112ba07959e
장지은 <wkdwldms2001@naver.com>
2026-07-10 11:25:19 +0900

    ✨ feat(#156): 로딩 박스 인디케이터 추가

--- app/src/main/java/com/a32b/plant/presentation/core/component/LoadableScreen.kt
+++ app/src/main/java/com/a32b/plant/presentation/core/component/LoadableScreen.kt
@@ -1,14 +1,18 @@
 package com.a32b.plant.presentation.core.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import com.a32b.plant.core.base.BaseViewModel
-import com.a32b.plant.presentation.theme.background
 
 @Composable
 fun LoadableScreen(
@@ -18,7 +22,14 @@ fun LoadableScreen(
     val isLoading by viewModel.isLoading.collectAsState()
 
     if (isLoading) {
-        Box(modifier = Modifier.fillMaxSize().background(background))
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .background(Color.Gray.copy(alpha = 0.3f))
+                .pointerInput(Unit){ detectTapGestures {  } }, //백그라운드 클릭 불가하게 처리
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(color = Color.LightGray)
+        }
     } else {
         content()
     }
