a8484219cd8273d19c3e7e2e315f60601bbaf9ac
장지은 <wkdwldms2001@naver.com>
2026-07-24 15:41:48 +0900

    ✨ : AppError 유저 정보 조회 실패 추가

--- app/src/main/java/com/a32b/plant/domain/error/AppError.kt
+++ app/src/main/java/com/a32b/plant/domain/error/AppError.kt
@@ -7,6 +7,7 @@ sealed class AppError : Throwable() {
 
     data class Network(override val message: String = "네트워크 연결이 원활하지 않습니다.") : AppError()
     data class Auth(override val message: String = "인증에 실패했습니다. 다시 로그인해주세요.") : AppError()
+    data class UnknownUser(override val message: String = "인증 정보를 가져오지 못했습니다. 다시 시도해주세요.") : AppError()
     data class Email(override val message: String = "이메일 형식이 올바르지 않습니다.") : AppError()
     data class Password(override val message: String = "비밀번호 형식이 올바르지 않습니다.") : AppError()
     data class Server(override val message: String = "서버에 오류가 발생했습니다.") : AppError()
