ad26595a4f8df49f35a5e52ed834a5e92c2ca8ae
장지은 <wkdwldms2001@naver.com>
2026-08-17 16:05:18 +0900

    chore : 기존 커뮤니티 활동 파일 롤백 및 바뀐 이름으로 변경

--- app/src/main/java/com/a32b/plant/presentation/community/viewmodel/CommunityActivityViewModel.kt
+++ app/src/main/java/com/a32b/plant/presentation/community/viewmodel/CommunityActivityViewModel.kt
@@ -1,8 +1,60 @@
 package com.a32b.plant.presentation.community.viewmodel
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import androidx.lifecycle.viewModelScope
+import com.a32b.plant.domain.type.ActivityType
+import com.a32b.plant.domain.model.CommunityActivity
+import com.a32b.plant.domain.repository.CommunityRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
+data class CommunityActivityUiState(
+    val selected: String = ActivityType.POST,
+    val activities: List<CommunityActivity> = emptyList()
+)
+sealed class CommunityActivityEvent{
+    data class NavigateToCommunityDetail(val postId: String) : CommunityActivityEvent()
+
+}
 @HiltViewModel
-class CommunityActivityViewModel @Inject constructor() {
+class CommunityActivityViewModel @Inject constructor(
+    private val repository: CommunityRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(CommunityActivityUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _eventChannel = Channel<CommunityActivityEvent>(Channel.BUFFERED)
+    val event = _eventChannel.receiveAsFlow()
+
+    private var collectJob: Job? = null
+    init {
+        loadActivity(_uiState.value.selected)
+    }
+    fun onSelectedChange(type: String) {
+        _uiState.update { it.copy(selected = type) }
+        loadActivity(type)
+    }
+
+    fun loadActivity(selected: String) {
+        collectJob?.cancel() // 이전 구독 취소
+        collectJob = viewModelScope.launch {
+            repository.getActivityList(selected)
+                .collect { list ->
+                    _uiState.update { it.copy(activities = list) }
+                }
+        }
+    }
+
+    fun moveToCommunityDetail(postId: String){
+        viewModelScope.launch {
+            _eventChannel.send(CommunityActivityEvent.NavigateToCommunityDetail(postId))
+        }
+    }
 }
