33682c57c3b8a9996002509ba86244691e6e23ce
장지은 <wkdwldms2001@naver.com>
2026-09-14 13:50:50 +0900

    ♻️ : 다크모드 여부 전역에서 활용할 수 있게 변경

---
+++ app/src/main/java/com/a32b/plant/presentation/theme/LocalDarkTheme.kt
@@ -0,0 +1,5 @@
+package com.a32b.plant.presentation.theme
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalIsDarkTheme = staticCompositionLocalOf<Boolean> { error("LocalIsDarkTheme is not provided") }
