8c9d73b8970f717208674a779097983a6f04a8fd
장지은 <wkdwldms2001@naver.com>
2026-09-23 18:12:12 +0900

    ♻️ : 기본 글씨 크기 17 -> 15

--- app/src/main/java/com/a32b/plant/presentation/theme/Type.kt
+++ app/src/main/java/com/a32b/plant/presentation/theme/Type.kt
@@ -49,7 +49,7 @@ fun plantTypography(colorScheme: ColorScheme): Typography{
         //기본 글씨 볼드용
         titleMedium = TextStyle(
             fontFamily = bold,
-            fontSize = 17.sp,
+            fontSize = 15.sp,
             lineHeight = 23.sp,
             color = colorScheme.onSurface
         ),
@@ -69,7 +69,7 @@ fun plantTypography(colorScheme: ColorScheme): Typography{
         //기본 글씨 - 커뮤니티의 게시글 본문 등 기타 모든 일반 글씨 작성 시
         bodyMedium = TextStyle(
             fontFamily = basic,
-            fontSize = 17.sp,
+            fontSize = 15.sp,
             lineHeight = 23.sp,
             color = colorScheme.onSurface
         ),
